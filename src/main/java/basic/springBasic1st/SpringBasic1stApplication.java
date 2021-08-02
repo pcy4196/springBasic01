ef1a2e3bef50a1f574a57e5e93aae3aeff74ac9a
PCY <pcy4196@naver.com>
2021-08-03 01:09:10 +0900

    1st commit project

---
+++ src/main/java/basic/springBasic1st/SpringBasic1stApplication.java
@@ -0,0 +1,13 @@
+package basic.springBasic1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasic1stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasic1stApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basic/springBasic1st/SpringBasic1stApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.springBasic1st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasic1stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
