8375720d263718be113021602f290861a7b4ec22
PCY <pcy4196@naver.com>
2021-08-15 00:08:18 +0900

    8th Push -- Ch05. 회원관리 예제 -- 1. 간단한 회원 웹MVC 구현(memmory 방식)

--- src/main/java/basic/springBasic1st/controller/MemberController.java
+++ src/main/java/basic/springBasic1st/controller/MemberController.java
@@ -1,8 +1,14 @@
 package basic.springBasic1st.controller;
 
+import basic.springBasic1st.domain.Member;
 import basic.springBasic1st.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -19,5 +25,31 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberservice) {
         this.memberService = memberservice;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String createMemeberForm(MemberForm form) {
+        System.out.println("getName : " + form.getName());
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member name : " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String listMember(Model model) {
+        List<Member> listMembers = memberService.findMembers();
+        model.addAttribute("listMembers", listMembers);
+        return "members/memberList";
     }
 }
